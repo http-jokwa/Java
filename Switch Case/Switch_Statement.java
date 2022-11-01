033e9e6ef9eea27275a68733be137b5c9e0001a0
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-01 12:44:53 +0800

    Add files via upload

---
+++ Switch Case/Switch_Statement.java
@@ -0,0 +1,27 @@
+package SwitchCase;
+
+public class Switch_Statement {
+
+	public static void main(String[] args) {
+		
+		
+		int number = 2;
+		
+		switch (number) {
+		
+		case 1 : 
+			System.out.println("This is number " + number);
+			break;
+			
+		case 2 :
+			System.out.println("This is number " + number);
+			break;
+			
+		default :	
+			System.out.println("This number is greater than 1 and 2 ");
+			break;	
+		}
+		
+	}
+
+}
