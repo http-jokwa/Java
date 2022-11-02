0a57ae716b9f9f1f3dc9ee1bbf372370c2bffcfa
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-02 17:46:24 +0800

    Add files via upload

---
+++ Java Operators/Arithmetic.java
@@ -0,0 +1,35 @@
+package Operators;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+	
+		/*
+		 * Arithmetic Operator used to perform arithmetic operations
+		 * + Addition
+		 * - Subtraction
+		 * * Multiplication
+		 * / Division
+		 * % Modulo (Remainder)
+		 */
+
+		int x = 20, y = 30;
+		
+		//addition
+		System.out.println(x + y); // output : 50
+		
+		//subtraction
+		System.out.println(x - y); // output : -10
+		
+		//multiplication
+		System.out.println(x * y); // output : 600
+		
+		//division
+		System.out.println(x / y); //output : 0
+		
+		//remainder or modulo
+		System.out.println(10 % 3); // output : 1
+		
+	}
+
+}
