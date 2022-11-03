cdf7c9c51d93eaee946669bb716f4fbf9670d92e
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-03 11:45:11 +0800

    Add files via upload

---
+++ Conditional Statement/Ternary_Statement.java
@@ -0,0 +1,16 @@
+package Conditional_Statement;
+
+public class Ternary_Statement {
+
+	public static void main(String[] args) {
+	
+		
+		int grade = 81, passing = 75;
+
+		
+		String result = (grade >= passing) ? "Pass" : "Fail";
+		
+		System.out.println(result); //output : Pass
+	}
+
+}
