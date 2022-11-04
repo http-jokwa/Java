eae4c960c770a91d7c8058c34cb5640775584605
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-04 09:43:15 +0800

    Add files via upload

---
+++ Loop/forEach.java
@@ -0,0 +1,26 @@
+package Loops;
+
+public class forEach {
+    public static void main(String args[]) {
+         
+         String [] names = {"Joschua", "Ordonio", "BSIT"};
+         
+			
+			
+         for(String val : names ) {
+             System.out.println(val);
+         }
+			
+			/* 
+					for(Data type for container : Array name) {
+							System.out.println(container name);	
+						}
+			
+			
+			For each loop will print all the element 
+			inside the array.
+			If you are confused in identifying the size of 
+			array, use for each loop.
+			*/
+    }
+}
