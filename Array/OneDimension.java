0035e73c7e1176266cd6b72539cd59c53e3e0f58
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-05 21:24:19 +0800

    Create OneDimension.java

---
+++ Array/OneDimension.java
@@ -0,0 +1,15 @@
+public class OneDimension {
+  
+ public static void main(String [] args) { 
+  
+
+   int [] number = {1,2,3,4,5};
+   
+    System.out.println(number[0]); //output : 1
+    System.out.println(number[1]); //output : 2
+    System.out.println(number[2]); //output : 3
+    System.out.println(number[3]); //output : 4
+    System.out.println(number[4]); //output : 5
+   
+ }
+}
