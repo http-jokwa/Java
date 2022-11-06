596339bb3eac2c08b52ebc2ca45ceb386ea276d9
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-06 18:24:38 +0800

    Update OneDimension.java

--- Array/OneDimension.java
+++ Array/OneDimension.java
@@ -5,11 +5,19 @@ public class OneDimension {
 
    int [] number = {1,2,3,4,5};
    
+   //to call the element, arrayname[index]
+   
     System.out.println(number[0]); //output : 1
     System.out.println(number[1]); //output : 2
     System.out.println(number[2]); //output : 3
     System.out.println(number[3]); //output : 4
     System.out.println(number[4]); //output : 5
+   
+   
+   //you can also reassign the element value of array
+   
+    number[0] = 12; //now the index zero with the value of 1 will now 12
+   System.out.println(number[0]); //output : 12
    
  }
 }
