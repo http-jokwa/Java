7c5426cc64fdcca515c731a297afdf6e8902e512
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-11 20:23:20 +0800

    Create Frame.java

---
+++ GUI/Frame.java
@@ -0,0 +1,14 @@
+
+import javax.swing.*;
+
+public class Frame {
+
+	public static void main(String[] args) {
+		
+		JFrame f = new JFrame("Hello");
+		
+		f.setVisible(true);
+		f.setSize(500, 500);
+	}
+
+}
