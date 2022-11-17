db9ac77ef814df15f339e431a0a3626d476b7938
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-17 23:38:42 +0800

    Update Label.java

--- GUI/Label.java
+++ GUI/Label.java
@@ -6,15 +6,13 @@ public class Label {
 	public static void main(String[] args) {
 		
 		JFrame f = new JFrame("Hello");	//create Java Frame object
-    JLabel jl = new JLabel("This is Label Text");
+   	        JLabel jl = new JLabel("This is Label Text"); //create Java Label object
 		
 		f.setVisible(true); // to make your frame appear/show/visible
 		f.setSize(500, 500); //set the dimension of your frame
     		f.add(jl); //we add label inside the frame
     		jl.setVisible(True); // we set visible the text
 		//we can also align the label using the setBounds() method
-    
-    
+  
 	}
-
 }
